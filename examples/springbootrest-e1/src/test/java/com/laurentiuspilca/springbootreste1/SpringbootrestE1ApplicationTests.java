1d75e69ec7e5c7d28a0fce5fff07d1278f7f5fb9
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2023-03-26 15:50:34 -0400

    Rest Api

---
+++ examples/springbootrest-e1/src/test/java/com/laurentiuspilca/springbootreste1/SpringbootrestE1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurentiuspilca.springbootreste1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootrestE1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
